ec3471de26e611474507f5ceebe85bf637154ac1
inaridarkfox4231 <sea_gull6389-4m@ka.tnc.ne.jp>
2022-05-15 16:02:09 +0900

    調整

--- sanmoku/src/index.js
+++ sanmoku/src/index.js
@@ -32,48 +32,21 @@ class Square extends React.Component {
   }
 */
   class Board extends React.Component {
-    constructor(props){
-      super(props);
-      this.state = {
-        squares: Array(9).fill(null),
-        xIsNext: true, // 先手がXを書き込む
-      }
-    }
-    handleClick(i){
-      // イミュータブルにすることで履歴が取得できるなど数々の恩恵を得られる
-      const squares = this.state.squares.slice(); // コピーを作っている！（指定しないsliceで）
-      // 決着がついてる場合、もしくはクリックしたsquareが描画済みの場合は早期にreturnする
-      if(calculateWinner(squares) || squares[i]){
-        return;
-      }
-      squares[i] = (this.state.xIsNext ? 'X' : 'O');
-      this.setState({
-        squares: squares,
-        xIsNext: !this.state.xIsNext
-      });
-    }
+    /* constructorを削除 */
     renderSquare(i) {
       return (
         <Square
-          value = {this.state.squares[i]}
-          onClick = {() => this.handleClick(i)}
+          value = {this.props.squares[i]}
+          onClick = {() => this.props.onClick(i)}
         />
       ); // value = {i}とすることでvalueが渡される
     }
     // なるほど複数渡すときは半角スペース区切りなのね
   
     render() {
-      const winner = calculateWinner(this.state.squares); // 勝者を表示
-      let status;
-      if(winner){
-        status = 'Winner ' + winner;
-      }else{
-        status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
-      }
-  
+      /* Gameコンポーネントで表示するように仕様変更 */
       return (
         <div>
-          <div className="status">{status}</div>
           <div className="board-row">
             {this.renderSquare(0)}
             {this.renderSquare(1)}
@@ -95,15 +68,114 @@ class Square extends React.Component {
   }
   
   class Game extends React.Component {
+    constructor(props){
+      super(props);
+      // hand: 最後に打たれた手の情報
+      this.state = {
+        history: [{
+          squares: Array(9).fill(null),
+          hand: null
+        }],
+        stepNumber:0,
+        xIsNext: true
+      }
+    }
+    handleClick(i){
+      // イミュータブルにすることで履歴が取得できるなど数々の恩恵を得られる
+      const history = this.state.history.slice(0, this.state.stepNumber + 1); // この時点ではまだstepNumberが更新されてないので1を足す
+      const current = history[history.length - 1];
+      const squares = current.squares.slice(); // コピーを作っている！（指定しないsliceで）
+      // 決着がついてる場合、もしくはクリックしたsquareが描画済みの場合は早期にreturnする
+      if(calculateWinner(squares) || squares[i]){
+        return;
+      }
+      squares[i] = (this.state.xIsNext ? 'X' : 'O');
+      const playerIsX = this.state.xIsNext; // プレイヤーはXですよの情報
+      const currentHand = {row: Math.floor(i / 3), col: i % 3, isX: playerIsX}; // そのターンにおかれた手数
+      this.setState({
+        history: history.concat([{ // concatは元の配列をmutateしない. pushでは無くこれを使うと。
+          squares: squares,
+          hand: currentHand
+        }]),
+        stepNumber: history.length,
+        xIsNext: !playerIsX
+      });
+    }
+    jumpTo(step){
+      this.setState({
+        stepNumber: step,
+        xIsNext: (step % 2) === 0
+      });
+    }
     render() {
+      const history = this.state.history;
+      const current = history[this.state.stepNumber];
+      const winner = calculateWinner(current.squares);
+
+      // 現在の手までの履歴表示
+      const historyUntilCurrent = this.state.history.slice(1, this.state.stepNumber+1);
+      const historyOfHands = historyUntilCurrent.map((step, eachMove) => {
+        const hand = historyUntilCurrent[eachMove].hand;
+        const handInfo = (hand.isX ? 'X' : 'O') + ": (" + hand.row + ", " + hand.col + ")";
+        return <li key = {eachMove}>{handInfo}</li> // keyが必要だそうで...追加です。
+      });
+
+      const moves = history.map((step, move) => {
+        // descはdescriptionの意味でよく用いられますね
+        const desc = move ? 
+          'Go to move #' + move:
+          'Go to game start';
+        // keyの設定...よくわかんないけど何だろ、修正不能な識別子のようなものらしいです。
+        // タグ内での条件分岐は即時関数で表現すればいいみたい（参考：https://www.yoheim.net/blog.php?q=20180409）
+        // {(() => {if(move === this.state.stepNumber){ return <div>ほげ～～</div> }})()} こんな感じ
+        // もしくは関数コンポーネント...を、使う？？
+        // {条件&&コンポーネント}ってやると条件がtrueのときだけ描画されるんだって
+        // {move === this.state.stepNumber && <div>ほげ～～</div>}
+        // でも最終的には関数コンポーネントのやり方になるかなぁ（内容が内容だけに）
+        // よくわかんないけどこれでいいみたい↓
+
+        // 練習だから関数で書いたけど短すぎるね...サクッと書いちゃえば、、でも練習だからね
+
+        function showHistoryOfHands(stepNumber){
+          if(move !== stepNumber){ return <div></div> }
+          // 以下で履歴を表示してみる感じ
+          // historyにはその辺の履歴が入ってないので、盤面の履歴とは別に、手の履歴が必要で、それを参照しないと表示できないわけです。
+          // とはいえ別に複雑なことは必要なくってhistoryの成分にプロパティ追加するだけでいいと思う
+          // rowとcolとばつまる情報の3つね
+          return <ol>{historyOfHands}</ol>
+        }
+        function showDescription(stepNumber, desc){
+          if(move !== stepNumber){ return <div>{desc}</div>; }
+          return <b>{desc}</b>;
+        }
+
+        return (
+          <li key = {move}>
+            <button onClick = {() => this.jumpTo(move)}>
+              {showDescription(this.state.stepNumber, desc)}
+            </button>
+            {showHistoryOfHands(this.state.stepNumber)}
+          </li>
+        );
+      });
+
+      let status;
+      if(winner){
+        status = 'Winner ' + winner;
+      }else{
+        status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
+      }
       return (
         <div className="game">
           <div className="game-board">
-            <Board />
+            <Board
+              squares = {current.squares}
+              onClick = {(i) => this.handleClick(i)}
+            />
           </div>
           <div className="game-info">
-            <div>{/* status */}</div>
-            <ol>{/* TODO */}</ol>
+            <div>{status}</div>
+            <ol>{moves}</ol>
           </div>
         </div>
       );
